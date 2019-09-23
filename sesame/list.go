db050ae440bd8abd0aaa238e8cba71ab3d7cc45a
Ryoma Kai <legknmj@icloud.com>
2019-09-23 20:46:52 +0900

    bugfix of list api

--- sesame/list.go
+++ sesame/list.go
@@ -10,16 +10,12 @@ type Sesame struct {
 	Nickname string
 }
 
-type Sesames struct {
-	Sesames []Sesame
-}
-
-func (c *Client) GetSesameList() (*Sesames, error) {
+func (c *Client) GetSesameList() ([]Sesame, error) {
 	resp, err := c.get(listPath)
 	if err != nil {
 		return nil, err
 	}
 
-	var result Sesames
-	return &result, c.decodeJSON(resp, &result)
+	result := make([]Sesame, 0)
+	return result, c.decodeJSON(resp, &result)
 }
